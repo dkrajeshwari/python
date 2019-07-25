5a4cab219726e6560758fa6e76064af313905e48
D k Rajeshwari <53164760+dkrajeshwari@users.noreply.github.com>
2019-07-25 16:19:41 +0530

    Delete occ.py

--- occ.py
+++
@@ -1,8 +0,0 @@
-names=["raji","sony","arpi","madhu","raj","yash"]
-c_names={}
-for name in names:
-    if c_names.get(name)==None:
-        c_names[name]=1
-    else:
-        c_names[name]+=1
-print(c_names)            
